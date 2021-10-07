e9c722ea7c442464af1ef1994768c78e873fb5c5
shubh baghel <78901584+shubhdo42@users.noreply.github.com>
2021-10-07 07:57:28 +0530

    fixed issue #1

--- src/Home.js
+++ src/Home.js
@@ -16,12 +16,14 @@ function Home() {
                    image ="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Wireless/Pranjal/MSO/update/DesktopGateway_CategoryCard_379X304._SY304_CB639966368_.jpg
                    "
                    rating={4}
+                   id ={1}
                    />
                    <Product title="Up to 65% off | TVs & appliances"
                    price={29999}
                    image ="https://images-eu.ssl-images-amazon.com/images/G/31/img21/TVs/Jupiter21/LATV/R01_CC_379X304._SY304_CB639977399_.jpg
                    "
                    rating={5}
+                   id={2}
                    />
 
                   
@@ -34,18 +36,21 @@ function Home() {
                    image ="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Fashion/Event/Gateway/Jupiter_21/PC__BTF_PFF/Rev_1/Mens-Clothing_._SY116_CB640931643_.jpg
                    "
                    rating={4}
+                   id ={3}
                    />
                    <Product title="Up to 50% off | Alexa & Fire TV devices"
                    price={1999}
                    image ="https://images-eu.ssl-images-amazon.com/images/G/31/img17/AmazonDevices/2019/Jupiter-21/PC5_1x._SY116_CB640379960_.jpg
                    "
                    rating={4}
+                   id ={4}
                    />
                    <Product title="Up to 70% off | Books, toys, grooming & more"
                    price={3999}
                    image ="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Books/092020/jupiter/gw/books-1x2._SY116_CB639940730_.jpg
                    "
                    rating={4}
+                   id ={5}
                    />
                    
 
